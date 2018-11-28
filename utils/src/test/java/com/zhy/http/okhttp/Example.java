7e8dfe741cdf7c7f3aff61217801614de8e089ba
kigkrazy <kigkrazy@163.com>
2018-11-28 13:54:40 +0800

    字符串编码转换

--- utils/src/test/java/com/zhy/http/okhttp/Example.java
+++ utils/src/test/java/com/zhy/http/okhttp/Example.java
@@ -3,6 +3,8 @@ package com.zhy.http.okhttp;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 import okhttp3.Response;
 
@@ -12,8 +14,9 @@ public class Example {
         Response response = OkHttpUtils
                 .get()
                 .url("http://2018.ip138.com/ic.asp")
+                .proxy(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 1080)))
                 .build()
                 .execute();
-        System.out.println("the respon : "  + response);
+        System.out.println("the ip addr : "  + new String(response.body().bytes(), "GB2312"));
     }
 }
