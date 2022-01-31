a16e75265d11bed130a69987a1d584566c0d8f65
kigkrazy <kigkrazy@gmail.com>
2022-01-31 10:58:25 +0800

    增加

---
+++ utils/src/main/java/com/zhy/http/okhttp/utils/StrUtil.java
@@ -0,0 +1,15 @@
+package com.zhy.http.okhttp.utils;
+
+public class StrUtil {
+    /**
+     * 字符串是否为空，空的定义如下:<br>
+     * 1、为null <br>
+     * 2、为""<br>
+     *
+     * @param str 被检测的字符串
+     * @return 是否为空
+     */
+    public static boolean isEmpty(CharSequence str) {
+        return str == null || str.length() == 0;
+    }
+}
